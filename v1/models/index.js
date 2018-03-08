8a460211f50ab4be021c538e08ef3cb52af6222e
Alfred Boateng <programmingwithalfred@gmail.com>
2018-03-08 14:11:18 +0000

    debug mode added to mongoose

--- v1/models/index.js
+++ v1/models/index.js
@@ -1,6 +1,7 @@
 /* setting up the database storage */
 var mongoose = require("mongoose");
 
+mongoose.set("debug", true);
 mongoose.connect("mongodb://localhost/todo_v1");
 
 mongoose.Promise = Promise;
